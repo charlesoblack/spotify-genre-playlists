89d5915dd93f9d2e4eca1fe24b1dee083d973096
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-07-13 15:51:49 +0200

    working with multiprocessing list

--- get-tracks.py
+++ get-tracks.py
@@ -20,9 +20,9 @@ def api_request(url, headers, params):
     if response.status_code == 200:
         data = response.json()
     else:
-        raise RuntimeError('received {}, {}, {}'.format(url,
-                                                        response.status_code,
-                                                        response.text))
+        return 'received {}, {}, {}'.format(url,
+                                            response.status_code,
+                                            response.text)
     return data
 
 
@@ -34,16 +34,23 @@ def api_get_tracks(mng, q):
     headers = {'Authorization': 'Bearer {}'.format(bearer_token)}
     base_url = "https://api.spotify.com/v1/me/tracks"
     request_url = base_url
-    params = {'limit': 10}
+    params = {'limit': 50}
+
+    print('getting tracks', flush=True)
 
     while request_url:
         data = api_request(request_url, headers, params)
+
+        if isinstance(data, str):
+            mng.append('TERMINATE')
+            raise RuntimeError(data)
+
         for item in data['items']:
             q.put(item['track']['id'])
 
-        request_url = None  # data['next']
+        request_url = data['next']
 
-    mng.put('SAVED TRACKS DONE')
+    mng.append('SAVED TRACKS DONE')
     return
 
 
@@ -54,33 +61,46 @@ def api_get_track_features(mng, q, results):
     bearer_token = os.environ['SPOTIFYTOKEN']
 
     tracks = []
+    tracks_processed = 0
+    print('getting track features', flush=True)
     while True:
-        while len(tracks) < 10:
+        while len(tracks) < 100:
             try:
                 tracks.append(q.get(block=False))
             except Empty:
-                pass
+                if 'SAVED TRACKS DONE' in mng:
+                    break
 
         if tracks:
+            tracks_processed += len(tracks)
+            print(tracks_processed, flush=True)
             headers = {'Authorization': 'Bearer {}'.format(bearer_token)}
             base_url = "https://api.spotify.com/v1/audio-features"
             params = {'ids': ','.join(tracks)}
 
             data = api_request(base_url, headers, params)
 
+            if isinstance(data, str):
+                mng.append('TERMINATE')
+                raise RuntimeError(data)
+
             for feature_set in data['audio_features']:
-                results.put(feature_set)
+                results.append(feature_set)
             tracks = []
+
+        if 'SAVED TRACKS DONE' in mng:
             break
 
-    mng.put('FEATURES DONE')
+    mng.append('FEATURES DONE')
     return
 
 
 if __name__ == '__main__':
+    import pickle
+
     q = multiprocessing.Queue()
-    mng = multiprocessing.Queue()
-    results = multiprocessing.Queue()
+    results = []
+    mng = multiprocessing.Manager().list()
 
     api_get_saved = multiprocessing.Process(target=api_get_tracks,
                                             args=(mng, q))
@@ -93,18 +113,20 @@ if __name__ == '__main__':
     processes = [api_get_saved, api_get_features]
 
     while any(process.is_alive() for process in processes):
-        try:
-            event = mng.get(block=False, timeout=1)
-            if event == 'SAVED TRACKS DONE':
-                print('saved tracks done', flush=True)
-                api_get_saved.terminate()
-                api_get_saved.join()
-            elif event == 'FEATURES DONE':
-                print('features done', flush=True)
-                api_get_features.terminate()
-                api_get_features.join()
-        except:  # noqa
-            pass
-        pass
+        if 'SAVED TRACKS DONE' in mng and api_get_saved.is_alive():
+            print('saved tracks done', flush=True)
+            api_get_saved.terminate()
+            api_get_saved.join()
+        if 'FEATURES DONE' in mng and api_get_features.is_alive():
+            print('features done', flush=True)
+            api_get_features.terminate()
+            api_get_features.join()
+        if 'TERMINATE' in mng:
+            print('terminated', flush=True)
+            [process.terminate() for process in processes]
+            [process.join() for process in processes]
 
-    print(results.get())
+    result_dict = {result['uri']: result for result in results}
+
+    with open('user-data.pckl', 'wb') as f:
+        pickle.dump(result_dict, f, protocol=-1)
