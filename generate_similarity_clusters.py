0db87417341ab6f46b49a4ef48f4cd3b09f05ed0
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-07-13 19:09:23 +0200

    getting clusters from ipynb

---
+++ generate_similarity_clusters.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+
+def get_clusters(uris_dict):
+    # import random
+    # payload = [random.choice(list(uris_dict.keys())) for uri in range(10)]
+    payload = ['spotify:track:1CRS7nh71URmTUbKAvgqPg',
+               'spotify:track:4sFbojhVXQv7dBC9PVCcRn',
+               'spotify:track:6ECyKD17oExHDMkF5wVmUm',
+               'spotify:track:0F0MA0ns8oXwGw66B2BSXm',
+               'spotify:track:0oks4FnzhNp5QPTZtoet7c']
+    return [payload]
